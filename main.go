d31c0c573bdeec21700913ab1b510e8ef5c6a84a
jonathanch12 <164126841+jonathanch12@users.noreply.github.com>
2026-09-23 22:14:22 +0700

    Implement command routing for account operations

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"blu/services"
+)
+
+func main() {
+	command := os.Args[1]
+
+	switch command {
+	case "create_account":
+	case "transfer" :
+	case "add_deposit" :
+	case "accrue_interest":
+	default:
+	}
+}
